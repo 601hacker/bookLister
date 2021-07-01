2f3f3da4568fb9ddb291325ecbcf4cc1dfaacf73
601hacker <farhanishraq601@gmail.com>
2021-07-01 23:23:39 +0600

    Basic Layout done

---
+++ script.js
@@ -0,0 +1,18 @@
+// const main = document.querySelector('main');
+// const addButton = document.querySelector('header button');
+// const form = document.querySelector('main form');
+// const body = document.querySelector('body');
+// const cancelButton = document.querySelector('main form [type="button"]');
+
+// addButton.addEventListener('click', function() {
+//     form.style.display = 'block';
+//     body.style.zIndex = '7';
+//     body.style.backgroundColor = 'rgb(0, 0, 0, 0.9)';
+//     console.log('hello');
+// });
+
+// cancelButton.addEventListener('click', function() {
+//     form.style.display = 'none';
+//     body.style.backgroundColor = '';
+// });
+
